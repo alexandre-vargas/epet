59a23a42fa6146bd93acd936c029b2d19a09243d
Flavius <red_loo@msn.com>
2014-08-07 11:55:59 -0300

    criacao tabela marca

--- geral.sql
+++ geral.sql
@@ -170,4 +170,15 @@ CREATE TABLE `epet`.`menu` (
   `log_usuario_alt` VARCHAR(20) NULL,
   PRIMARY KEY (`id`));
   
+  CREATE TABLE `epet`.`marca` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(200) NULL,
+  `status` TINYINT NULL,
+  `log_data_insert` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP(),
+  `log_data_alt` TIMESTAMP NULL,
+  `log_usuario_insert` VARCHAR(20) NULL,
+  `log_usuario_alt` VARCHAR(20) NULL,
+  PRIMARY KEY (`id`));
+  
+  
   
